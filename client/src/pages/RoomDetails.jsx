026f4c528788e98df690dd2829273dc576295a92
Harshit2528 <harshitdongarwar2003@gmail.com>
2025-09-30 21:12:57 +0530

    made the my bookings page and room details page

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -12,7 +12,7 @@ const RoomDetails = () => {
     const room = roomsDummyData.find((room) => room._id === id);
     room && setRoom(room);
     room && setMainImage(room.images[0]);
-  }, []);
+  }, [id]);
   return room && (
     
       <div className="py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32">
